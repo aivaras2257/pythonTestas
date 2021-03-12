b97cbd721fff79f52235d2713e98b750fae73764
aivaras <aivaras735@gmail.com>
2021-03-12 12:53:34 +0200

    testas

---
+++ testas.py
@@ -0,0 +1,38 @@
+# pirma dalis
+# 1
+pirmas = int(10)
+antras = str("Sveiki")
+trecias = float(20.2)
+ketvirtas = bool(5)
+# 2
+list1 = {
+    "as":1,
+    "tu":2,
+    "jis":3
+}
+list2 = [1, 2, 3]
+list3 = ["as", "tu", "jis"]
+
+
+# 3
+# manoKintamasis
+# 4
+# manoKintamasis = float(21.2);
+# 5
+def function(food):
+    for x in food:
+        print(x)
+
+
+fruits = ["apple", "kiwi", "banana"]
+function(fruits)
+# antra dalis
+#6
+kintamasis = "IlgasSakinys"
+x = kintamasis[2:5]
+y = kintamasis[7:9]
+print(x, y)
+# x =gas
+# y = ki
+#7
+#mutable galima keisti , o not mutable negalima
